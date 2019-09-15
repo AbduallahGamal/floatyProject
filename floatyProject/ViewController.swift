5e5c11500fe89d0cd7b71f05894e9f8f0c2ee87f
Abduallah <abduallahgamal@gmail.com>
2019-09-15 19:38:27 +0200

    Initial Commit

---
+++ floatyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  floatyProject
+//
+//  Created by Abdalla on 9/15/19.
+//  Copyright © 2019 edu.data. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
